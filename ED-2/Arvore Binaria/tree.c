3f9f29046da398e4fc81a299b5aeecb62abb2eb0
danielsouzza <daniel.lima.souzza@gmail.com>
2022-12-04 18:28:38 -0300

    Fix bug

--- ED-2/Arvore Binaria/tree.c
+++ ED-2/Arvore Binaria/tree.c
@@ -22,11 +22,12 @@ Node * initialize(){
 
 Person * buildPerson(){
     Person * person = malloc(sizeof(Person));
+    
+    printf("Nome: ");
+    fgets((*person).name,30,stdin);
     printf("CPF: ");
     scanf("%d",&(*person).cpf);
     getchar();
-    printf("Nome: ");
-    fgets((*person).name,30,stdin);
     printf("JOB: ");
     fgets((*person).job,50,stdin);
 
@@ -36,8 +37,8 @@ Person * buildPerson(){
 void treePrint(Node * root){
     if(root != NULL){
         printf("\n===========================================\n");
-        printf("CPF: %d\n", root->person->cpf);
         printf("NOME: %s",root->person->name);
+        printf("CPF: %d\n", root->person->cpf);
         printf("JOB: %s",root->person->job);
         treePrint(root->left);
         treePrint(root->right);
@@ -54,8 +55,8 @@ void treeFree(Node * root){
 
 void print_Person(Person * person){
     printf("\n===========================================\n");
-    printf("CPF: %d\n", person->cpf);
     printf("NOME: %s",person->name);
+    printf("CPF: %d\n", person->cpf);
     printf("JOB: %s",person->job);
 }
 
@@ -93,53 +94,62 @@ Node * insert_by_name(Node * root, Person * person){
     return root;
 }
 
-void search_by_cpf(Node * root, int cpf){
-    if(root != NULL){
+char * search_by_cpf(Node * root, int cpf){
+    char * name;
+    if(root){
         if(cpf == root->person->cpf){
             print_Person(root->person);
+            return root->person->name;
         }else if(cpf > root->person->cpf){
-            search_by_cpf(root->right, cpf);
+            name = search_by_cpf(root->right, cpf);
         }else if(cpf < root->person->cpf){
-            search_by_cpf(root->left, cpf);
+            name = search_by_cpf(root->left, cpf);
         }
     }else{
         printf("\n===========================================\n");
         printf("Pessoa não encontrada!!");
+        return name;
     }
 }
 
 
 
 
-void search_by_name(Node * root, char * name){
-    if(root != NULL){
+int search_by_name(Node * root, char * name){
+    int cpf;
+    if(root){
         if(strcmp(name,root->person->name) == 0){
             print_Person(root->person);
+            return root->person->cpf;
         }else if(name[0] > root->person->name[0]){
-            search_by_name(root->right, name);
+            cpf = search_by_name(root->right, name);
         }else if(name[0] < root->person->name[0]){
-            search_by_name(root->left, name);
+            cpf = search_by_name(root->left, name);
         }
     }else{
         printf("\n===========================================\n");
         printf("Pessoa não encontrada!!");
+        return cpf;
     }
 }
 
 Node * remove_cpf(Node * root, int cpf){
 
-    if(root != NULL){
+    if(root){
         if(cpf == root->person->cpf){
             if(root->left == NULL && root->right == NULL){
                 free(root);
                 return NULL;
             }else if (root->left != NULL && root->right != NULL){
                 Node * aux = root->left;
+                Person * tmp;
                 while (aux->right != NULL)
                 {
                     aux = aux->right;
                 }
+                tmp = root->person;
                 root->person = aux->person;
+                aux->person = tmp;
                 root->left = remove_cpf(root->left,cpf);
                 return root;   
 
@@ -163,18 +173,21 @@ Node * remove_cpf(Node * root, int cpf){
 }
 
 Node * remove_name(Node * root, char * name){
-    if(root != NULL){
+    if(root){
         if(strcmp(name,root->person->name) == 0){
             if(root->left == NULL && root->right == NULL){
                 free(root);
                 return NULL;
             }else if (root->left != NULL && root->right != NULL){
                 Node * aux = root->left;
+                Person * tmp;
                 while (aux->right != NULL)
                 {
                     aux = aux->right;
                 }
+                tmp = root->person;
                 root->person = aux->person;
+                aux->person = tmp;
                 root->left = remove_name(root->left,name);
                 return root;   
 
@@ -200,14 +213,13 @@ Node * remove_name(Node * root, char * name){
 
 
 void menu(){
-    printf("[1] - Inserir com cpf\n");
-    printf("[2] - Inserir com nome\n");
-    printf("[3] - Remover com cpf\n");
-    printf("[4] - Remover com nome\n");
-    printf("[5] - Buscar com cpf\n");
-    printf("[6] - Buscar com nome\n");
-    printf("[7] - Imprimir Arvore ordenado por cpf\n");
-    printf("[8] - Imprimir Arvore ordenado por nome\n");
+    printf("[1] - Inserir\n");
+    printf("[2] - Remover com cpf\n");
+    printf("[3] - Remover com nome\n");
+    printf("[4] - Buscar com cpf\n");
+    printf("[5] - Buscar com nome\n");
+    printf("[6] - Imprimir Arvore ordenado por cpf\n");
+    printf("[7] - Imprimir Arvore ordenado por nome\n");
     printf("[0] - Exit\n");
 }
 
@@ -229,40 +241,44 @@ int main(){
 
         switch (choose){
         case 1:
-            root_cpf = insert_by_cpf(root_cpf, buildPerson());
+            Person * person = buildPerson();
+            root_cpf = insert_by_cpf(root_cpf, person);
+            root_name = insert_by_name(root_name, person);
             break;
         case 2:
-            root_name = insert_by_name(root_name, buildPerson());
-            break;
-        case 3:
             printf("Digite o cpf: ");
             scanf("%d", &cpf);
+            char * nome = search_by_cpf(root_cpf,cpf);
             root_cpf = remove_cpf(root_cpf,cpf);
+            root_name = remove_name(root_name,nome);
             getchar();getchar();
             break;
-        case 4:
-            printf("Digete o nome: ");
+        case 3:
+            printf("Digite o nome: ");
             fgets(name,30,stdin);
+            cpf = search_by_name(root_name,name);
+            root_cpf = remove_cpf(root_cpf,cpf);
             root_name = remove_name(root_name,name);
+            getchar();
             break;
-        case 5:
+        case 4:
             printf("Digite o cpf: ");
             scanf("%d", &cpf);
             search_by_cpf(root_cpf,cpf);
             getchar();getchar();
             break;
-        case 6:
+        case 5:
             printf("Digite o name: ");
             fgets(name,30,stdin);
             search_by_name(root_name,name);
             getchar();getchar();
             break;
-        case 7:
+        case 6:
             system("clear");
             treePrint(root_cpf);
             getchar();
             break;
-        case 8:
+        case 7:
             system("clear");
             treePrint(root_name);
             getchar();
